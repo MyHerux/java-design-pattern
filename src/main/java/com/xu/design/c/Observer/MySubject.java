971868a95bf8448a4130f5de733fe03d2ee9dfab
myherux <heroxu123@gmail.com>
2016-09-28 21:04:04 +0800

    观察者模式(Observer)

---
+++ src/main/java/com/xu/design/c/Observer/MySubject.java
@@ -0,0 +1,11 @@
+package com.xu.design.c.Observer;
+
+public class MySubject extends AbstractSubject {
+  
+    @Override  
+    public void operation() {  
+        System.out.println("update self!");  
+        notifyObservers();  
+    }  
+  
+}  
---
+++ src/main/java/com/xu/design/c/Observer/Observer.java
@@ -0,0 +1,5 @@
+package com.xu.design.c.Observer;
+
+public interface Observer {
+    public void update();  
+}
